4e150be6fa8047886d225bacf6e658c27752d55c
phoenixSP <pal00007@umn.edu>
2020-03-03 10:56:12 -0600

    ADDING PROBLEMS IN AGGREGATION

---
+++ aggregation/earnings-of-employees.sql
@@ -0,0 +1,10 @@
+/* not working but can't figure out why */
+
+SELECT *
+FROM (
+    SELECT Months * Salary, COUNT(*)
+    FROM Employee
+    GROUP BY (Months * Salary)
+    ORDER BY Months * Salary DESC
+    )
+WHERE ROWNUM = 1;
